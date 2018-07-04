7ad768c74d59fa3b41625da86fcd57213d9b0820
denglt <ltdeng@hotmail.com>
2018-07-04 17:47:34 +0800

    eureka demo

---
+++ eureka-server/src/main/java/dlt/study/eureka/ExampleEurekaService2.java
@@ -0,0 +1,61 @@
+package dlt.study.eureka;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.appinfo.providers.EurekaConfigBasedInstanceInfoProvider;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaClientConfig;
+
+/**
+ * Sample Eureka service that registers with Eureka to receive and process requests.
+ * This example just receives one request and exits once it receives the request after processing it.
+ *
+ */
+public class ExampleEurekaService2 {
+
+    private static ApplicationInfoManager applicationInfoManager;
+    private static EurekaClient eurekaClient;
+
+    private static synchronized ApplicationInfoManager initializeApplicationInfoManager(EurekaInstanceConfig instanceConfig) {
+        if (applicationInfoManager == null) {
+            InstanceInfo instanceInfo = new EurekaConfigBasedInstanceInfoProvider(instanceConfig).get();
+            applicationInfoManager = new ApplicationInfoManager(instanceConfig, instanceInfo);
+        }
+
+        return applicationInfoManager;
+    }
+
+    private static synchronized EurekaClient initializeEurekaClient(ApplicationInfoManager applicationInfoManager, EurekaClientConfig clientConfig) {
+        if (eurekaClient == null) {
+            eurekaClient = new DiscoveryClient(applicationInfoManager, clientConfig);
+        }
+
+        return eurekaClient;
+    }
+
+
+    /**
+     *  -Deureka.client.props=eureka-service2
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        DynamicPropertyFactory configInstance = DynamicPropertyFactory.getInstance();
+        ApplicationInfoManager applicationInfoManager = initializeApplicationInfoManager(new MyDataCenterInstanceConfig());
+        EurekaClient eurekaClient = initializeEurekaClient(applicationInfoManager, new DefaultEurekaClientConfig());
+
+        ExampleServiceBase exampleServiceBase = new ExampleServiceBase(applicationInfoManager, eurekaClient, configInstance);
+        try {
+            exampleServiceBase.start();
+        } finally {
+            // the stop calls shutdown on eurekaClient
+            exampleServiceBase.stop();
+        }
+    }
+}
+
