54fd1b690a3131eff9cca79d492baa8a28ae474d
denglt <ltdeng@hotmail.com>
2018-11-22 10:07:55 +0800

    no log

---
+++ eureka-server/src/main/java/com/study/springcloud/eurekaserver/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.study.springcloud.eurekaserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+/**
+ * @Description:
+ * @Package: com.springboot.actuator
+ * @Author: denglt
+ * @Date: 2018/9/18 下午4:53
+ * @Copyright: 版权归 HSYUNTAI 所有
+ */
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin").password("eurekaserver")
+                .authorities("ADMIN");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+
+}
